09e6c60db0d26e59ee7c701c921e9ec7d63d63d9
ljhee92 <weilainib7@gmail.com>
2024-12-21 15:18:10 +0900

    chore: service package 위치 변경

--- src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -2,10 +2,10 @@ package jpabook.jpashop.domain.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
