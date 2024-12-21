c47c1b447bdaf88fa25f625fc7f0d2a85aec350a
ljhee92 <weilainib7@gmail.com>
2024-12-21 14:57:18 +0900

    feat: 상품 서비스 구현, 테스트

---
+++ src/test/java/jpabook/jpashop/domain/service/ItemServiceTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ItemServiceTest {
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @Rollback(value = false)
+    public void 상품등록() throws Exception {
+        // given
+        Item book = new Book();
+        book.setName("book1");
+
+        // when
+        Long savedId = itemService.saveItem(book);
+
+        // then
+        assertThat(book).isEqualTo(itemRepository.findOne(savedId));
+    }
+}
