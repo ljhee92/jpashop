613d587352eee1aa4db939fdf1988035ebaa9270
ljhee92 <weilainib7@gmail.com>
2025-01-19 18:34:08 +0900

    feat: 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,16 +7,46 @@ import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result MembersV2() {
+        List<MemberDTO> list = memberService.findMembers().stream()
+                .map(member -> new MemberDTO(member.getName()))
+                .toList();
+
+        return new Result(list.size(), list);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> { // Collection을 바로 반환하지 않고 한 번 감싸주면 유연성을 높일 수 있다.
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -44,7 +74,6 @@ public class MemberApiController {
 
     @Data
     static class UpdateMemberRequest {
-
         private String name;
     }
 
